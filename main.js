70b63a229774ac12e55a7220ee5b86917e0851df
evandenmark <evanlewisdenmark@gmail.com>
2021-01-13 10:22:20 -0800

    added tooltip

--- main.js
+++ main.js
@@ -42,6 +42,21 @@ function initialLoad(){
 }
 
 function drawLocationCircles(locations){
+
+	var tooltip = d3.select('body')
+		.append('g')
+		.attr('id', 'tooltip')
+		.attr('style', 'position: absolute; opacity: 0; font-family: Helvetica');
+
+	var tooltipName = tooltip.append('div')
+								.attr('id', 'tooltipName')
+								.attr('style', 'font-size: 16px');
+
+	var tooltipDate = tooltip.append('div')
+								.attr('id', 'tooltipDate')
+								.attr('style', 'font-size: 12px');;
+
+
 	mapSvg.selectAll(".locations")
 				.data(audioData)
 				.join(
@@ -57,8 +72,10 @@ function drawLocationCircles(locations){
 					return "translate(" + r +','+q + ")";
 				})
 				.attr("class", "locations")
+				.attr('id', function(d){return d.index})
 		    	.attr("r", 10)
 		    	.on("click", function(d){
+
 
 		    		//handle audio
 		    		if (currentlyPlaying){
@@ -83,20 +100,44 @@ function drawLocationCircles(locations){
 		    		}
 
 		    	})
-		    	.on('mouseover', function(d, i) {
-			      d3.select(this)
-			        .transition()
-			        .duration(100)
-			        .attr('r', 20)
-			        .attr('fill', '#ff0000');
-			    })
-			    .on('mouseout', function(d, i) {
-			      d3.select(this)
-			        .transition()
-			        .duration(100)
-			        .attr('r', 10)
-			        .attr('fill', '#7FCEFF');
-			    })
+		    	// .on('mouseover', mouseover )
+		    	.on('mouseover', function(d,i){
+		    		//create tooltip
+		    		d3.select('#tooltip')
+		    			.transition()
+		    			.duration(200)
+		    			.style('opacity', 1)
+
+		    		d3.select("#tooltipName")
+		    			.text(d.name)
+
+		    		d3.select("#tooltipDate")
+		    			.text(d.date)
+
+		    		//make circle big
+		    		d3.select(this)
+				        .transition()
+				        .duration(100)
+				        .attr('r', 20)
+				        .attr('fill', '#ff0000');
+					})
+
+			    .on('mouseout', function(){
+			    	d3.select('#tooltip').style('opacity', 0)
+
+			    	//make circle small
+			    	d3.select(this)
+			    		.transition()
+			    		.duration(100)
+			    		.attr('r', 10)
+			    	})
+
+			    .on('mousemove', function() {
+					d3.select('#tooltip')
+					.style('left', d3.event.pageX+20 + 'px')
+					.style('top', d3.event.pageY+1 + 'px')
+					})
+
 		    	.style("fill", '#7FCEFF')
 		    	.style("stroke", '#0674B9')
 		    	.style("stroke-width", 3);
